fc5ba0f4eb2f9b5c76594e3ded51f83bc4367db0
adrianburgos <adrian.burgos.colas@gmail.com>
2021-11-02 00:11:27 -0400

    starting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const plugin = require('tailwindcss/plugin')
+
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
@@ -5,7 +7,7 @@ module.exports = {
     extend: {},
   },
   variants: {
-    extend: {backgroundColor: ['active'],boxShadow: ['active'],scale: ['active']},
+    extend: {scale:['group-hover'],display:['group-hover'],backgroundColor: ['active'],boxShadow: ['active'],scale: ['active']},
   },
-  plugins: [],
+  plugins: []
 }
