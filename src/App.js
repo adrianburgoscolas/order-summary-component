fee5a5ca268b709cb9189ae05a1488b8a221ff8c
adrianburgos <adrian.burgos.colas@gmail.com>
2021-11-03 02:38:17 -0400

    reponsive mobile/desktop ok

--- src/App.js
+++ src/App.js
@@ -5,34 +5,31 @@ import planlogo from "./images/icon-music.svg"
 function App() {
   return (
     <div>
-      <div className="flex flex-col items-center justify-center h-screen App">
+      <div className="flex flex-col items-center justify-center h-screen overflow-scroll App">
         <div id='card' className="overflow-hidden rounded-3xl">
 
           <img className='rounded-none' src={illustration} alt="Audio content." />
 
-          <div id='car-content' className="flex flex-col p-12">
-            <div className='text-3xl font-bold text-center' id="header-text">Order Summary</div>
-            <div id='content-text' className='p-5 font-bold text-center'>
+          <div id='card-content' className="flex flex-col p-6 md:p-12">
+            <div className='p-2 font-extrabold text-center md:p-0 md:text-3xl' id="header-text">Order Summary</div>
+            <div id='content-text' className='p-2 text-center md:p-5 md:font-bold '>
               You can now listen to millions of songs, audiobooks, and podcasts on
               any device anywhere you like!
             </div>
-            <div id='plan' className='flex p-7 rounded-xl'>
+            <div id='plan' className='flex p-4 my-5 md:my-0 md:p-7 rounded-xl'>
               <img id='logo' src={planlogo} alt='Plan logo'/>
               <div id='plan-text' className='px-5'>
-                <div id='plan-header' className='font-extrabold'>Annual Plan</div>
-                <div id='plan-price' className='font-bold'>$59.99/year</div>
+                <div id='plan-header' className='text-sm font-extrabold md:text-base'>Annual Plan</div>
+                <div id='plan-price' className='font-bold '>$59.99/year</div>
               </div>
-              <div id='plan-change' className='self-center flex-auto font-bold text-right underline transition-all'><a href='#'>Change</a></div>
+              <div id='plan-change' className='self-center flex-auto font-bold text-right underline transition-colors'><a href='#'>Change</a></div>
             </div>
-            <button id='payment' className='block p-3.5 my-7 rounded-xl font-bold transition-all'>Proceed to Payment</button>
+            <button id='payment' className='block p-3 md:p-3.5 my-2 md:my-7 rounded-xl font-bold transition-colors'>Proceed to Payment</button>
 
-            <button id='cancel' className='block font-extrabold transition-all'>Cancel Order</button>
-          </div>
+            <button id='cancel' className='block mt-4 font-extrabold transition-colors md:mt-0'>Cancel Order</button>
+          </div><br />
 
-          
-          
-        </div>
-        <footer className='absolute bottom-16' id='app-footer'>
+          <footer className='absolute pl-2 m-auto text-xs md:pl-5 md:text-base' id='app-footer'>
           Challenge by{" "}
           <a
             href="https://www.frontendmentor.io?ref=challenge"
@@ -51,6 +48,9 @@ function App() {
           </a>
           .
         </footer>
+          
+        </div>
+        
       </div>
     </div>  
   );
